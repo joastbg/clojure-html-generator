2be51093ec56cb710a55d8234abde8a0e207603a
Johan Astborg <johan@futurumedge.se>
2012-10-19 22:48:32 +0200

    higher order functions for cleaner code

--- html.clj
+++ html.clj
@@ -2,45 +2,51 @@
 ; Johan Astborg 2012
 
 ; helpers
-(def stag (fn[t] (reduce str ["<",t,">"])))
-(def etag (fn[t] (reduce str ["</",t,">"])))
-(def mtag (fn[t,b] (reduce str [(stag t),b,(etag t)])))
+(defn m-reduce [l] (reduce str l))
+
+(def stag (fn[t] (m-reduce ["<",t,">"])))
+(def etag (fn[t] (m-reduce ["</",t,">"])))
+(def mtag (fn[t,b] (m-reduce [(stag t),b,(etag t)])))
+
+(defn multag-n [n] (fn [& args] (mtag n (m-reduce args))))
+(defn unitag-n [n] (fn [s] (mtag n s)))
 
 ; tags
-(defn html [& args] (mtag "html" (reduce str args)))
-(defn head [& args] (mtag "head" (reduce str args)))
-(defn body [& args] (mtag "body" (reduce str args)))
-(defn title [& args] (mtag "title" (reduce str args)))
-(defn footer [& args] (mtag "footer" (reduce str args)))
-(defn p [& args] (mtag "p" (reduce str args)))
+(def html (multag-n "html"))
+(def head (multag-n "head"))
+(def body (multag-n "body"))
+(def title (multag-n "title"))
+(def footer (multag-n "footer"))
+(def p (multag-n "p"))
 
 ; h1-h6
-(def h1 (fn[s] (mtag "h1" s)))
-(def h2 (fn[s] (mtag "h1" s)))
-(def h3 (fn[s] (mtag "h1" s)))
-(def h4 (fn[s] (mtag "h1" s)))
-(def h5 (fn[s] (mtag "h1" s)))
-(def h6 (fn[s] (mtag "h1" s)))
+(def h1 (unitag-n "h1"))
+(def h2 (unitag-n "h2"))
+(def h3 (unitag-n "h3"))
+(def h4 (unitag-n "h4"))
+(def h5 (unitag-n "h5"))
+(def h6 (unitag-n "h6"))
 
 ; text-level semantics
-(def b (fn[s] (mtag "b" s)))
-(def big (fn[s] (mtag "big" s)))
-(def mark (fn[s] (mtag "mark" s)))
-(def i (fn[s] (mtag "i" s)))
-(def strong (fn[s] (mtag "strong" s)))
-(def small (fn[s] (mtag "small" s)))
-(def strike (fn[s] (mtag "strike" s)))
+(def b (unitag-n "b"))
+(def i (unitag-n "i"))
+(def big (unitag-n "big"))
+(def mark (unitag-n "mark"))
+(def small (unitag-n "small"))
+(def strong (unitag-n "strong"))
+(def strike (unitag-n "strike"))
 
 ; hr
 (def hr (fn[] "<hr/>"))
 
 ; a href
-(def ahref (fn[l,t] (reduce str ["<a href='",l,"'>",t,"</a>"])))
+(def ahref (fn[l,t] (m-reduce ["<a href='",l,"'>",t,"</a>"])))
 
 ; ul, ol and li
 (def li (fn[s] (mtag "li" s)))
-(defn ol [& args] (mtag "ol" (reduce str (map li args))))
-(defn ul [& args] (mtag "ul" (reduce str (map li args))))
+
+(defn ol [& args] (mtag "ol" (m-reduce (map li args))))
+(defn ul [& args] (mtag "ul" (m-reduce (map li args))))
 
 ; a test page
 (println 
